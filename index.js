61d6bda576de03dcc3190026c876399dbdcae880
Tiago Marques <thymarques@gmail.com>
2021-11-26 19:54:23 -1000

    adding the fileName on the downloaded file

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  await res.status(200).send("Welcome to youtube downloader service!");
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to youtube downloader service!");
 });
 
 app.post("/download", async (req, res) => {
-  let videoUrl = req.body.url || `http://www.youtube.com/watch?v=aqz-KE-bpKQ`;
-
-  await ytdl(videoUrl).pipe(fs.createWriteStream("video.mp4"));
+  let videoUrl = req.body.url;
 
   let info = await ytdl.getBasicInfo(videoUrl);
   let title = info.videoDetails.title;
+  let fileName = title.replaceAll(" ", "_");
   let videoId = info.videoDetails.videoId;
   let duration = info.videoDetails.lengthSeconds;
+
+  if (!info) {
+    res.status(400).send({ error: "URL invalid, try again!" });
+  }
+
+  ytdl(videoUrl).pipe(fs.createWriteStream(fileName));
   //res.download('video.mp4')
   //res.setHeader("Content-Disposition", "attachment; video.mp4");
   res.status(200).send({
